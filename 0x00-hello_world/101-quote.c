1791f8e6b657d7cf48d471098d18d7d1bc078519
kelvin <kitskelly38@gmail.com>
2023-02-16 20:18:11 +0300

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/*
+ * File: 101-quote.c
+ * Auth: Christopher K. Oyier
+ * Desc: A program that prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ *       followed by a new line, to the standard error.
+ */
+
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+/* Prints the text to the standard error */
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
