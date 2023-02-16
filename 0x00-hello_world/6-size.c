44558de13f8587722588f6801cb2921ee8a99988
kelvin <kitskelly38@gmail.com>
2023-02-16 19:44:07 +0300

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints the size of various types on the computer it is compiled and run on.
+ *Prints size of the computer it is compiled and run on.
  *
  * Return: Always 0 (Success)
  */
