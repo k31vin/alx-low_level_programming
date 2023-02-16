5b198dc1178b32ec623fb71b52e69307a587734c
kelvin <kitskelly38@gmail.com>
2023-02-16 19:35:32 +0300

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the required string with proper grammar using
+ *              the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
