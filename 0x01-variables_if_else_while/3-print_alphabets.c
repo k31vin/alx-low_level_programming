7c7d28213fbcbea9d6d68f731abee4dcc7d8d5f2
kelvin <kitskelly38@gmail.com>
2023-02-17 19:31:58 +0300

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char ch2 = 'A';
+
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
+	while (ch2 <= 'Z')
+	{
+	putchar(ch2);
+	ch2++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
