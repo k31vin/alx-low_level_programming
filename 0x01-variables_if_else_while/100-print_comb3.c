32da4ebd4cab3f7afcec62108c5cc6afa8d8e906
kelvin <kitskelly38@gmail.com>
2023-02-17 20:21:04 +0300

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int first_digit, second_digit;
-	
+
 	for (first_digit = 0; first_digit < 9; first_digit++)
 	{
 	for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
- 	{
+	{
 	putchar(first_digit + '0');
 	putchar(second_digit + '0');
 
