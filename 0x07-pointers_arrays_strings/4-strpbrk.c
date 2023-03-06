645ffec2b7c484cbc6d07b4e89e2eca233370cc1
kelvin <kitskelly38@gmail.com>
2023-03-07 01:01:47 +0300

    task 4 $ 11

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,59 +1,36 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strpbrk - Searches a string for any of a set of bytes.
- * @s: string to searched
- * @accept: set of bytes to search for
- *
- * Return: If a set is matched - a pointer to the matched byte.
- *         If no set is matched - NULL.
+ * *_strpbrk - function declaration
+ * @s: string to search
+ * @accept: bytes to search
+ * Return: pointer or NULL
+ */
+
+char *_strpbrk(char *s, char *accept);
+
+/**
+ * *_strpbrk - function definition
+ * @s: the string to be searched
+ * @accept: bytes to search for
+ * Description: searches for a string of a set of bytes
+ * Return: pointer to matched byte or NULL if no match
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+	int str_index;
+	int sub_str_index;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (str_index = 0; s[str_index] != '\0'; str_index++)
 	{
-		for (n = 0; accept[n] != '\0'; n++)
+		for (sub_str_index = 0; accept[sub_str_index] != '\0'; sub_str_index++)
 		{
-			if (s[i] == accept[n])
+			if (s[str_index] == accept[sub_str_index])
 			{
-				return (s + i);
-
+				return (s + str_index);
 			}
 		}
 	}
-
-	return (NULL);
-#include "main.h"
-#include <stdio.h>
-
-/**
- * _strpbrk - Searches a string for any of a set of bytes.
- * @s: string to searched
- * @accept: set of bytes to search for
- *
- * Return: If a set is matched - a pointer to the matched byte.
- *         If no set is matched - NULL.
- */
-
-char *_strpbrk(char *s, char *accept)
-{
-	int i, n;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (s[i] == accept[n])
-			{
-				return (s + i);
-
-			}
-		}
-	}
-
-	return (NULL);
-}}
+	return (0);
+}
